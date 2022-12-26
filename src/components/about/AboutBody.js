90e237be83a27c9f7841985e46394401127fc6e4
Dalia Jaber <daliajaber96@gmail.com>
2022-12-26 02:11:50 +0200

    modified responsivness about header and body working

--- src/components/about/AboutBody.js
+++ src/components/about/AboutBody.js
@@ -3,13 +3,14 @@ import therapistImg from '../../images/therapist_.svg';
 
 export default function AboutBody() {
   return (
-    <div className="aboutBody bg-cyan-100 sm:max-h-fit sm:h-fit sm:w-88   sm:px-20 lg:px-32 py-12 lg:flex lg:justify-center">
-      <div className="imgView flex lg:justify-end lg:align-middle sm:justify-center sm:align-middle sm:w-full bg-red-200">
+    <div className="aboutBody flex flex-col gap-3 justify-center items-center p-8 bg-cyan-100 lg:flex-row">
+    
+      <div className="imgView">      
         <img src={therapistImg} alt="therapist" className='w-60	h-60' />
       </div>
-      <div className="body bg-red-300 w-full h-full">
-        <h2>Our Funding</h2>
-        <p>
+      <div className="body flex flex-col justify-center items-center p-8 gap-4 lg:w-[36rem]">
+        <h2 className="text-4xl lg:text-2xl lg:self-start">Our Funding</h2>
+        <p className="">
           Healing was founded by Payam Abubakr in 2021. It was called Healing
           Online, and started as a blog and an online community where some of
           the world&lsquo;s therapists shared their research and ideas. We
