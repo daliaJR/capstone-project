61f2028823be549f6c1b2bf1dc40456bffa007b7
Dalia Jaber <daliajaber96@gmail.com>
2022-12-26 20:36:14 +0200

    add one line after import in app

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import './App.css';
+
 function App() {
   return (
     <div className="">
