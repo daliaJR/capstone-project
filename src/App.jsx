6704f299c2f7b2cb0aba7d02905dd299dd50b244
Mhamad6000 <mhamadothman6000@gmail.com>
2022-12-27 20:21:49 +0300

    with thankyou page

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,17 @@ import Home from './components/Home';
 import Resource from './pages/Resource';
 import './App.css';
 import Contact from './features/counter/Contact';
+import ThankYou from './pages/ThankYou';
 
 function App() {
   return (
-    <div className="">
+    <div className="py-8">
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/resource" element={<Resource />} />
+          <Route path="/thankyou" element={<ThankYou />} />
         </Routes>
       </Router>
     </div>
