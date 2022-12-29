3a3062bfc60909e554ca1c31593613c036941f02
noor <noorridha028@gmail.com>
2022-12-29 15:27:37 +0300

    added some styling to the log in component

---
+++ src/components/Signup.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Simg from '../images/Simg.png';
+
+export default function Signup() {
+  return (
+    <div className="maindiv">
+      <div className="leftdiv">
+        <img
+          className="relative w-[674px] h-[320px] object-right"
+          src={Simg}
+          alt="signup"
+        />
+      </div>
+    </div>
+  );
+}
