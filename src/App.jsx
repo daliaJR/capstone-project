78a345b0f1611ee78ee375a1f528513801733776
Mhamad6000 <mhamadothman6000@gmail.com>
2022-12-29 20:37:44 +0300

    version 1 of buyTicket and payment

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ import Resource from './pages/Resource';
 import './App.css';
 import Contact from './features/counter/Contact';
 import ThankYou from './pages/ThankYou';
-import Footer from "./components/Footer"
+import Footer from './components/Footer';
+import Payment from './pages/Payment';
+import BuyTicket from './pages/BuyTicket';
 
 function App() {
   return (
@@ -17,6 +19,8 @@ function App() {
           <Route path="/resource" element={<Resource />} />
           <Route path="/thankyou" element={<ThankYou />} />
           <Route path="/footer" element={<Footer />} />
+          <Route path="/payment" element={<Payment />} />
+          <Route path="/buyticket" element={<BuyTicket />} />
         </Routes>
       </Router>
     </div>
---
+++ src/pages/BuyTicket.jsx
@@ -0,0 +1,27 @@
+import { React } from 'react';
+import Tickets from '../components/Tickets';
+
+export default function BuyTicket() {
+  return (
+    <div>
+      <section className="font-poppins py-6">
+        <div className="xl:max-w-6xl max-w-sm md:max-w-2xl px-5 mx-auto ">
+          <h2 className="text-5xl uppercase leading-18 ">select card</h2>
+          <p className="text-black opacity-50 text-lg font-normal mb-20">
+            Please select the card you want to buy the tickets with
+          </p>
+          <Tickets />
+          <div className="flex flex-col mt-20">
+            <h2 className="text-3xl uppercase mb-16">
+              Click confirm to use the selected card to purchase 5 tickets for
+              10$
+            </h2>
+            <button className="blue_button mb-16 mx-auto" type="button">
+              CONFIRM PURCHASE
+            </button>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
