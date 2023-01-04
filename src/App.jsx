162856b6a178db3fff1ba29f4237db7c8c098f7a
sara ahmed <sara.bakr.ahmad@gmail.com>
2023-01-04 20:27:14 +0300

    added changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom'
 import './App.css';
 import Navbar from './components/navigation/Navbar';
-import Requirementspage from './components/Requirements/requirementspage';
+
 
 function App() {
   return (
-    <Router>
-    <Routes>
-      
-      <Route path="/Navbar" element={<Navbar />} />
-      <Route path="/requirements" element={<Requirementspage />} />
-      
-    </Routes>
-  </Router>
+    <BrowserRouter>
+      <Navbar />
+    </BrowserRouter>
   );
 }
 
