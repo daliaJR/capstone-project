b69ed167a1c4327a51b87c521d09c201a8c54583
noor <noorridha028@gmail.com>
2023-01-05 19:57:35 +0300

    login

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import Resource from './pages/Resource';
 import './App.css';
 import Contact from './features/counter/Contact';
 import ThankYou from './pages/ThankYou';
-import Footer from "./components/Footer"
+import Footer from './components/Footer';
+import Login from './pages/Login';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Route path="/resource" element={<Resource />} />
           <Route path="/thankyou" element={<ThankYou />} />
           <Route path="/footer" element={<Footer />} />
+          <Route path="/Login" element={<Login />} />
         </Routes>
       </Router>
     </div>
