0799b3975f9470fd47ab6434c6638f98fe444343
Dalia Jaber <daliajaber96@gmail.com>
2023-01-17 19:14:13 +0200

    modify position test

--- src/__tests__/Position.test.js
+++ src/__tests__/Position.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import Position from '../components/Position';
 
-test('should render position component', async () => {
+test('should render position component', () => {
   render(<Position />);
   const section = screen.getByTestId("section-wrapper");
   expect(section).toBeInTheDocument();
