8b723a1c2a35d76ae4af4d042aaccd788727eaf9
Dalia Jaber <daliajaber96@gmail.com>
2023-01-17 18:36:29 +0200

    add test for position component related to career page

--- src/__tests__/Position.test.js
+++ src/__tests__/Position.test.js
@@ -6,11 +6,3 @@ test('should render position component', async () => {
   const section = screen.getByTestId("section-wrapper");
   expect(section).toBeInTheDocument();
 });
-
-// test('should render position component', async () => {
-//   render(<Position />);
-//   const positionComponent = await screen.findAllByLabelText("position-section");
-//   // const positionComponent = await screen.findByTestId("position-section", undefined, {timeout: 3000});
-//   expect(positionComponent).toBeInTheDocument();
-// });
-
