817931ef5446a8f2fae6b3c67e40603f51247786
Mhamad6000 <mhamadothman6000@gmail.com>
2023-01-23 20:40:36 +0300

    removed some test files

--- src/components/PhilosophySection.jsx
+++ src/components/PhilosophySection.jsx
@@ -3,7 +3,7 @@ import { data } from '../staticData/careerPage';
 
 export default function PhilosophySection() {
   return (
-    <div className="bg-light-yellow text-dark-gray ">
+    <div className="bg-light-yellow text-dark-gray " data-testid="div-wrapper">
       <div className="max-w-7xl mx-auto flex flex-col  py-4 px-5">
         <h1 className="md:text-4xl text-2xl ">OUR HIRING PHILOSOPHY</h1>
         <p className="md:text-xl text-sm">{data.philosophySectionText}</p>
