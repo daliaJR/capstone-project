315f3ae86b6c85569dd7a3123e6e0826d6014779
Mhamad6000 <mhamadothman6000@gmail.com>
2023-01-23 20:59:43 +0300

    fixed the tests

--- src/__tests__/RequirementsPage.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import RequirementsPage from '../pages/RequirementsPage';
-
-test('should render Thank you page', () => {
-  render(<RequirementsPage />);
-  const requirementsPage = screen.getByTestId('requirements-page');
-  expect(requirementsPage).toBeInTheDocument();
-});
